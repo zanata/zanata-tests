d90ebccf5920d6335448c79f3faae8c14fffb7c8
Ding-Yi Chen <dchen@redhat.com>
2010-10-01 17:05:31 +1000

    W.I.P import.pl

--- selenium/src/perl/import.pl
+++ selenium/src/perl/import.pl
@@ -1,11 +1,45 @@
 #!/usr/bin/env perl
 # Import the sample projects
 # Ensure it runs on RHEL5
+
+use constant FLIES_PYTHON_CLIENT_EXE => "flies";
+use constant FLIES_MAVEN_CLIENT_EXE => "mvn";
+use constant PUBLICAN_EXE => "publican";
 use 5.008_008;
 use strict "vars";
+use Getopt::Std;
+use Pod::Usage;
+use File::Path qw(make_path remove_tree);
+sub VERSION_MESSAGE {
+    my $fh = shift;
+    print $fh "flies import script 0.1.0 \n";
+}
+
+sub HELP_MESSAGE{
+    my $fh = shift;
+    print $fh "Usage: $0 [-p] [-a action]\n";
+    print $fh "Options: p: Use flies python client.\n";
+    print $fh "         a: Perform only that action.\n";
+}
+
+# opt map
+my %opts=();
+getopts("pa:", \%opts);
+my $action=$opts{'a'};
+
 # Get script location
 my ($scriptDir)= ($0 =~ m|(.*)/|);
 require "${scriptDir}/manage_variable.pl";
+
+my $fliesPythonClient="";
+my $fliesMavenClient="";
+if ($opts{'p'}){
+    $fliesPythonClient=find_program(FLIES_PYTHON_CLIENT_EXE);
+    die "&FLIES_PYTHON_CLIENT_EXE not found in PATH" unless $fliesPythonClient;
+}else{
+    $fliesMavenClient=find_program(FLIES_MAVEN_CLIENT_EXE);
+    die "&FLIES_MAVEN_CLIENT_EXE not found in PATH" unless $fliesMavenClient;
+}
 
 my $fliesUrl=set_var_with_env('FLIES_URL');
 my $TEST_CONFIG_FILE=set_var_with_env('TEST_CONFIG_FILE','./test.cfg');
@@ -22,34 +56,74 @@ while ( <IN> ) {
     my $valret=$enval;
     if ($valret =~ s/^"(.*)"$/$1/ ){
 	$valret =~ s/\$\{([^{]*)\}/$ENV{$1}/g ;
-	print "enval=|${enval}| valret=|${valret}|\n";
+	#print "enval=|${enval}| valret=|${valret}|\n";
     }
     $ENV{$envar}=${valret};
-    print "var=$envar valret=|${valret}|\n";
+    #print "var=$envar valret=|${valret}|\n";
 }
 close IN;
 $ENV{FLIES_URL} = $fliesUrl if $fliesUrl;
 print "FLIES_URL=|$ENV{FLIES_URL}|\n";
 
-my $client=""
-# has_project <proj_id> <cachefile>
-sub has_project_python{
-    my ($proj_id, $cachefile) = @_;
-    if ($client eq "python"){
-	my $_ret=`$flies_pytho_client list | grep -e "Id:\s*${proj_id}"`
-	if ($_ret){
-	    return 1;
-	}
-	return 0;
-    }
-}
+my $publicanCmd= find_program(PUBLICAN_EXE);
 
-sub is_current_apikey_valid(){
-    my ($_apikey_file)=@_;
-    if ( -e $_apikey_file){
+
+# has_project <proj_id> <cachefile>
+sub has_project{
+    my ($proj_id, $cachefile) = @_;
+    my $_ret="";
+    if ($fliesPythonClient){
+	$_ret=`$fliesPythonClient list | grep -e "Id:\s*${proj_id}"`
+    }else{
+    }
+    if ($_ret){
 	return 1;
     }
     return 0;
 }
 
+my $apikey="";
+unless (is_current_apikey_valid("apikey.admin")){
+    system("./get_apikey.sh");
+}
+open(KEYFILE, "apikey.admin");
+$apikey=<KEYFILE>;
+close(KEYFILE);
+
+make_path($ENV{'SAMPLE_PROJ_DIR'});
+unlink $ENV{'FILES_PUBLICAN_LOG'};
+
+my @publicanProjects=split /\s/, $ENV{'PUBLICAN_PROJECTS'};
+foreach my $pProj (@publicanProjects){
+    print "Processing project ${pProj}:".$ENV{"${pProj}_NAME"}. "\n";
+    if (has_project(${pProj},"tmp0.html")){
+	unless ($action){
+	    print "  Flies already has this project, skip importing.\n";
+	    next;
+	}
+	print "  Flies has this project, start ${action}.\n";
+    }
+
+    my $clone_action="";
+    my $update_action="";
+    if ($ENV{"${pProj}_REPO_TYPE"} eq "git"){
+	$clone_action="git clone";
+	$update_action="git pull";
+    }elsif($ENV{"${pProj}_REPO_TYPE"} eq "svn"){
+	$clone_action="svn co";
+	$update_action="svn up";
+    }
+
+    my $proj_dir="$ENV{'SAMPLE_PROJ_DIR'}/$pProj";
+    unless( $action){
+	if (-d ${proj_dir}){
+	    print "    ${proj_dir} exists, updating.\n";
+	    system("cd ${proj_dir}; $update_action");
+	}else{
+	    print "    ${proj_dir} does not exist, clone now.\n";
+	    system("$clone_action ". $ENV{"${pProj}_URL"} . " ${proj_dir}");
+	}
+    }
+
+}
 
--- selenium/src/perl/manage_variable.pl
+++ selenium/src/perl/manage_variable.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 # Get variable from a setting file
-# And some utilities subroutine
+# And some utilities subroutines
 
 # set_var_with_env(env_name, default_value)
 sub set_var_with_env{
@@ -10,9 +10,17 @@ sub set_var_with_env{
 
 sub find_program{
     my ($progName) = @_;
-    my $_ret=`which ${[progName} 2>/dev/null`
+    my $_ret=`which ${progName} 2>/dev/null`;
     die "Error: ${progName} cannot be found is PATH!" unless $_ret;
     return $_ret;
+}
+
+sub is_current_apikey_valid(){
+    my ($_apikey_file)=@_;
+    if ( -e $_apikey_file){
+	return 1;
+    }
+    return 0;
 }
 
 
