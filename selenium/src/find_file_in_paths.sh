a2bd177ce05dea5d90f76037c2f8432c41c00c30
Ding-Yi Chen <dchen@redhat.com>
2010-11-03 17:05:48 +1000

    Find a way to use CMake without C/C++ compiler.
    --HG--
    rename : selenium/src/steer.cmake => selenium/src/CMakeLists.txt

--- selenium/src/find_file_in_paths.sh
+++ selenium/src/find_file_in_paths.sh
@@ -3,11 +3,13 @@
 
 PATTERN=$1
 SEARCH_PATHS=`echo $2 | sed -e 's/ /\\ /g' | sed -e 's/\([^\\]\);/\1 /g' | sed -e 's/\\;/;/g' `
-echo "SEARCH_PATHS=${SEARCH_PATHS}"
+#echo "SEARCH_PATHS=${SEARCH_PATHS}"
 
 for fileDir in ${SEARCH_PATHS} ;do
     dirs=`/bin/ls -d ${fileDir} 2>/dev/null`
+    #echo "dirs=${dirs}"
     for fileDirM in $dirs; do
+	#echo "fileDirM=${fileDirM}"
 	filePath=`find $fileDirM -name "${PATTERN}" -type f | head --lines=1`
 	if [ -n ${filePath} ]; then
 	    echo "${filePath}"
