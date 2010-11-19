4d2339ecbdbbe5a5005443ce5c7c14290c59ed44
Ding-Yi Chen <dchen@redhat.com>
2010-11-19 16:58:30 +1000

    Add prepare_project script

---
+++ scripts/prepare_project.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# Once complete successfully, a file named "$stamp" is touched.
+# If publican.cfg exists, this script does a publican update,
+# otherwise do noting except touching "$stamp"
+langs=$1
+stamp=$2
+
+if [ -e publican.cfg ]; then
+    sed -e "s/brand:.*//" publican.cfg > publican.cfg.striped
+    publican update_pot --config publican.cfg.striped \
+    && publican update_po --config publican.cfg.striped --langs ${langs}
+fi
+
+touch ${stamp}
+
