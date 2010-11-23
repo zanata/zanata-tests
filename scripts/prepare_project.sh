20b9c98540a29098307412b6bf528e902e6b52e9
Ding-Yi Chen <dchen@redhat.com>
2010-11-23 16:48:45 +1000

    [RHEL5] testing on RHEL5 box

--- scripts/prepare_project.sh
+++ scripts/prepare_project.sh
@@ -5,10 +5,12 @@
 langs=$1
 stamp=$2
 
-if [ -e publican.cfg ]; then
-    sed -e "s/brand:.*//" publican.cfg > publican.cfg.striped
-    publican update_pot --config publican.cfg.striped \
-    && publican update_po --config publican.cfg.striped --langs ${langs}
+if [ -e /usr/bin/publican ] ; then
+    if [ -e publican.cfg ]; then
+	sed -e "s/brand:.*//" publican.cfg > publican.cfg.striped
+	publican update_pot --config publican.cfg.striped \
+	&& publican update_po --config publican.cfg.striped --langs ${langs}
+    fi
 fi
 
 touch ${stamp}
