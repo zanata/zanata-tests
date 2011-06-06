231030d073e8f22f9280d58a8eb8bcb8377562c9
Ding-Yi Chen <dingyichen@gmail.com>
2011-06-07 01:07:24 +1000

    Fix the py rest test

--- scripts/generate_zanata_xml.sh
+++ scripts/generate_zanata_xml.sh
@@ -1,34 +1,45 @@
 #!/usr/bin/env sh
 scriptDir=`dirname $0`
 baseDir=$1
-proj=$2
-ver=$3
-zanataUrl=$4
-shift 4;
+zanataUrl=$2
+proj=$3
+shift 3;
 
+# ver="" means project only
+if [ -n "$1" ];then
+    ver=$1
+    shift
+else
+    ver=
+fi
 
 projDir=${baseDir}/${proj}/${ver}
 
 rm -f ${projDir}/zanata.xml
-cat >> ${projDir}/zanata.xml << END
+cat > ${projDir}/zanata.xml << END
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <config xmlns="http://zanata.org/namespace/config/">
     <url>${zanataUrl}</url>
     <project>${proj}</project>
-    <project-version>${ver}</project-version>
-    <locales>
 END
 
-_langs=`echo $1 | sed -e 's/;/ /g'`
+if [ -n "$ver" ]; then
+    echo "    <project-version>${ver}</project-version>" >> ${projDir}/zanata.xml
+    echo "    <locales>" >> ${projDir}/zanata.xml
 
-for l in ${_langs}; do
-    lDir=`${scriptDir}/find_valid_lang_dir.sh "$projDir" $l`
-    if [ -n "$lDir" ]; then
+    _langs=`echo $1 | sed -e 's/;/ /g'`
+
+    for l in ${_langs}; do
+	lDir=`${scriptDir}/find_valid_lang_dir.sh "$projDir" $l`
+	if [ -n "$lDir" ]; then
 	    echo "        <locale map-from=\"$lDir\">$l</locale>" >> ${projDir}/zanata.xml
-    fi
-done
+	fi
+    done
+    echo "    </locales>" >> ${projDir}/zanata.xml
+fi
+
+
 
 cat >> ${projDir}/zanata.xml << END
-    </locales>
 </config>
 END
