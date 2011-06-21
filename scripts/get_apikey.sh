27a7662f3c9214caf683c6340d326ad3b778cec1
Ding-Yi Chen <dingyichen@gmail.com>
2011-06-21 15:30:21 +1000

    [get_apikey.sh] Found that get_apikey.sh is currently broken due to the change of session.
    Still not fix yet.

--- scripts/get_apikey.sh
+++ scripts/get_apikey.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # Get the apikey
+
 USER=
 PASS=
 if [ $# -ge 1 ]; then
@@ -10,31 +11,49 @@ else
     PASS=admin
 fi 
 
-if [ -z "$ZANATA_URL" ]; then
-    source ./test.cfg
+# Environment's ZANATA_PROFILE should take precedence
+ZANATA_PROFILE_OVERRIDE=
+if [ -n ${ZANATA_PROFILE} ]; then
+	ZANATA_PROFILE_OVERRIDE=${ZANATA_PROFILE}
 fi
-echo "ZANATA_URL=$ZANATA_URL"
+source ./test.cfg
+# Restore ZANATA_PROFILE
+if [ -n ${ZANATA_PROFILE_OVERRIDE} ]; then
+	ZANATA_PROFILE=${ZANATA_PROFILE_OVERRIDE}
+fi
+echo "Using ZANATA_PROFILE ${ZANATA_PROFILE}"
+eval "ZANATA_URL=\${${ZANATA_PROFILE}_SERVER_BASE}\${${ZANATA_PROFILE}_SERVER_PATH}"
+echo "ZANATA_URL=${ZANATA_URL}"
 COOKIE_FILE=cookie.file
-curl --cookie-jar ${COOKIE_FILE} --output tmp0.html ${ZANATA_URL}/account/sign_in
+curl --location --cookie-jar ${COOKIE_FILE} --output tmp0.html ${ZANATA_URL}/account/sign_in
 
 # extract jsession id
-JSESSION_ID=`grep -m 1 '<form.*sign_in;jsessionid=' tmp0.html | sed -e 's/^.*sign_in//' | sed -e 's/".*//'`
-echo "JSESSION_ID=${JSESSION_ID}"
+#JSESSION_ID=`grep -m 1 '<form.*sign_in;jsessionid=' tmp0.html | sed -e 's/^.*sign_in//' | sed -e 's/".*//'`
+#echo "JSESSION_ID=${JSESSION_ID}"
+
+# extract form ViewState
+VIEW_STATE_ID=javax.faces.ViewState
+VIEW_STATE=`grep -m 1 name="\"${VIEW_STATE_ID}\"" tmp0.html | sed -e "s/^.*name=\"${VIEW_STATE_ID}\"[^>]*value=\"//" | sed -e 's/".*//'`
+echo "VIEW_STATE=$VIEW_STATE"
 
 # extract form signature
 FORM_SIGNATURE=`grep -m 1 'name="javax.faces.FormSignature"' tmp0.html | sed -e 's/^.*name="javax.faces.FormSignature"[ \t]*value="//' | sed -e 's/".*//'`
 echo "FORM_SIGNATURE=$FORM_SIGNATURE"
 
 
-CMD1="curl --cookie ${COOKIE_FILE} --cookie-jar ${COOKIE_FILE}  --data 'login=login' --data \"javax.faces.FormSignature=${FORM_SIGNATURE}\" --data \"login%3AusernameField%3Ausername=${USER}\" --data \"login%3ApasswordField%3Apassword=${PASS}\" --data \"login%3ArememberMeField%3ArememberMe=on&login%3Aj_id117=Sign+In&javax.faces.ViewState=j_id1\" --location --output tmp1.html ${ZANATA_URL}/account/sign_in${JSESSION_ID}"
-#echo "CMD1=$CMD1"
+CMD1="curl --cookie ${COOKIE_FILE} --cookie-jar ${COOKIE_FILE}  --data 'login=login' --data \"javax.faces.FormSignature=${FORM_SIGNATURE}\" --data \"login%3AusernameField%3Ausername=${USER}\" --data \"login%3ApasswordField%3Apassword=${PASS}\" --data \"login%3ArememberMeField%3ArememberMe=on&login%3ASign_in=Sign+In&javax.faces.ViewState=${VIEW_STATE_ID}\" --location --output tmp1.html ${ZANATA_URL}/account/sign_form"
+
+#CMD1="curl \"--cookie login=login;javax.faces.FormSignature=${FORM_SIGNATURE};login%3AusernameField%3Ausername=${USER};login%3ApasswordField%3Apassword=${PASS};login%3ArememberMeField%3ArememberMe=on;login:Sign_in=Sign+In;javax.faces.ViewState=${VIEW_STATE_ID}\" --cookie-jar ${COOKIE_FILE} --location --output tmp1.html ${ZANATA_URL}/account/sign_form"
+
+#${JSESSION_ID}"
+echo "CMD1=$CMD1"
 eval "$CMD1"
 
 curl --cookie ${COOKIE_FILE} --location --output tmp2.html ${ZANATA_URL}/profile/view
 
-csplit -f tmpc tmp2.html '%Your current API%'
+csplit -f tmpc tmp2.html '%Your current API key is%'
 ret=`head --lines=2 tmpc00 | grep '<code>' | sed -e 's|^.*<code>||' | sed -e 's|</code>.*||'`
-rm -f tmpc tmp?.html ${COOKIE_FILE}
+#rm -f tmpc tmp?.html ${COOKIE_FILE}
 export APIKEY_${USER}=${ret}
 eval echo APIKEY_${USER}=\${APIKEY_${USER}}
 echo ${ret} > apikey.${USER}
