42389b989335d17c5f94acdb0c15381401f61e30
Ding-Yi Chen <dingyichen@gmail.com>
2011-11-21 17:59:07 +1000

    UImap: Issue50: rest of the test suite (yet the GoSecurityGuide need to update in case it's not in the first page

--- selenium/src/functions/ui-map.js
+++ selenium/src/functions/ui-map.js
@@ -2,6 +2,7 @@
 // TO DO THIS! In order for it to come into play, a user extension must
 // construct the map in this way.
 var myMap = new UIMap();
+var manager = new RollupManager();
 
 // define UI elements common for all pages. This regular expression does the
 // trick. '^' is automatically prepended, and '$' is automatically postpended.
@@ -9,6 +10,11 @@ var myMap = new UIMap();
 // string, all backslashes must be escaped with an additional backslash. Also
 // note that the URL being matched will always have any trailing forward slash
 // stripped.
+/*************
+ * Definition
+ */
+
+const emptyEntryDisplayString="Click here to start translating";
 
 /*************
  * All pages
@@ -233,6 +239,8 @@ function webtranPages_get_locator(row){
     if (row < 0){
 	var lastNum= -1 -row;
 	return '//tr[contains(@class,"TableEditorRow")][position()=last()-'+lastNum+']';
+    }else if (row == 0){
+	return '//tr[contains(@class,"TableEditorRow")][contains(@class,"selected")]'
     }
     return '//tr[contains(@class,"TableEditorRow")]['+ row +']';
 }
@@ -244,8 +252,8 @@ myMap.addElement('webtranPages', {
     , args: [
 	{
 	    name: 'row'
-	    , description: 'Row number, start from  1, -1 is the last row'
-	    , defaultValues: [ -1, 1, 2, 3]
+	    , description: 'Row number, start from  1, -1 is the last row, 0 is current row'
+            , defaultValues: [ -1, 0, 1, 2, 3]
 	}
     ]
     , getLocator: function(args){
@@ -309,17 +317,36 @@ myMap.addElement('webtranPages', {
 	    + '</tr>'
 	    + '</table>'
     }
+    , testcase4: {
+	args: { row:0 }
+	,xhtml:
+	    '<table class="TableEditor"><colgroup></colgroup>'
+	    + '<tr><td></td><td></td></tr>'
+	    + '<tr class="TableEditorRow odd-row">'
+	    +     '<td class="TableEditorCell-Source"><table class="TableEditorSource"><tr><td>Source1</td></tr></table></td>'
+	    +     '<td class="TableEditorCell-Target"><span class="xml-text">Click here to start translating</span></td>'
+	    + '</tr>'
+	    + '<tr expected-result="1" class="TableEditorRow even-row selected highlighted">'
+	    +     '<td class="TableEditorCell-Source"><table class="TableEditorSource"><tr><td>Source2</td></tr></table></td>'
+	    +     '<td class="TableEditorCell-Target"><span class="xml-text">Click here to start translating</span></td>'
+	    + '</tr>'
+	    + '<tr class="TableEditorRow odd-row">'
+	    + 	  '<td class="TableEditorCell-Source"><table class="TableEditorSource"><tr><td>SourceLast</td></tr></table></td>'
+	    +     '<td class="TableEditorCell-Target"><span class="xml-text">Click here to start translating</span></td>'
+	    + '</tr>'
+	    + '</table>'
+    }
 });
 
 
 myMap.addElement('webtranPages', {
     name: 'targetCell'
-    , description: 'Select message by row number in table editor'
+    , description: 'Message target cell on given row number'
     , args: [
 	{
 	    name: 'row'
-	    , description: 'Row number, start from  1, -1 is the last row'
-	    , defaultValues: [ -1, 1, 2, 3]
+            , description: 'Row number, start from  1, -1 is the last row, 0 is current row'
+            , defaultValues: [ -1, 0, 1, 2, 3]
 	}
     ]
     , getLocator: function(args){
@@ -344,6 +371,268 @@ myMap.addElement('webtranPages', {
 	    +     '<td class="TableEditorCell-Target"><span class="xml-text">Click here to start translating</span></td>'
 	    + '</tr>'
 	    + '</table>'
+    }
+});
+
+myMap.addElement('webtranPages', {
+    name: 'copySrcButton'
+    , description: 'Copy from source button'
+    , args: [
+	{
+	    name: 'row'
+            , description: 'Row number, start from  1, -1 is the last row, 0 is current row'
+            , defaultValues: [ -1, 0, 1, 2, 3]
+	}
+    ]
+    , getLocator: function(args){
+	var row=args['row'];
+	return webtranPages_get_locator(row) + '//img[starts-with(@title,"Copy message")]';
+    }
+    , testcase1: {
+	args: { row:2 }
+	,xhtml:
+	    '<table class="TableEditor"><colgroup></colgroup>'
+	    + '<tr><td></td><td></td></tr>'
+	    + '<tr class="TableEditorRow odd-row">'
+	    +     '<td class="TableEditorCell-Source">'
+	    +         '<table class="TableEditorSource">'
+	    +            '<tr><td>Source1</td><td><img title="Copy message from"/></td></tr>'
+	    +         '</table>'
+	    +     '</td>'
+	    +     '<td class="TableEditorCell-Target"><span class="xml-text">Click here to start translating</span></td>'
+	    + '</tr>'
+	    + '<tr class="TableEditorRow even-row">'
+	    +     '<td class="TableEditorCell-Source">'
+	    +         '<table class="TableEditorSource">'
+	    +            '<tr><td>Source2</td><td><img expected-result="1" title="Copy message from"/></td></tr>'
+	    +         '</table>'
+	    +     '</td>'
+	    +     '<td class="TableEditorCell-Target"><span class="xml-text">Copy message from</span></td>'
+	    + '</tr>'
+	    + '<tr class="TableEditorRow odd-row">'
+	    +     '<td class="TableEditorCell-Source">'
+	    +         '<table class="TableEditorSource">'
+	    +            '<tr><td>Source1</td><td><img title="Copy message from"/></td></tr>'
+	    +         '</table>'
+	    +     '</td>'
+	    +     '<td class="TableEditorCell-Target"><span class="xml-text">Click here to start translating</span></td>'
+	    + '</tr>'
+	    + '</table>'
+    }
+});
+
+myMap.addElement('webtranPages', {
+    name: 'pageEntry'
+    , description: 'Page number entry'
+    , args: []
+    , locator: 'css=a[title^="Previous Page"]~input'
+});
+
+manager.addRollupRule({
+    name: 'sim_click'
+    , description: 'Simulate a click if normal click does not work'
+    , args: [
+        {
+            name: 'loc'
+            , description: 'locator to be click'
+        }
+    ]
+    , commandMatchers: [
+        {
+	    command: 'mouseOver'
+            , target: '.+'
+	    , updateArgs: function(command, args) {
+		args.loc=command.target;
+	        return args;
+	    }
+	}
+        ,{
+	    command: 'mouseDown'
+            , target: '.+'
+	    , updateArgs: function(command, args) {
+		args.loc=command.target;
+	        return args;
+	    }
+	}
+        ,{
+	    command: 'mouseUp'
+            , target: '.+'
+	    , updateArgs: function(command, args) {
+		args.loc=command.target;
+	        return args;
+	    }
+	}
+    ]
+    , getExpandedCommands: function(args) {
+        var commands = [];
+	commands.push({
+	    command: 'mouseOver'
+	    , target: args.loc
+	});
+	commands.push({
+	    command: 'mouseDown'
+	    , target: args.loc
+	});
+	commands.push({
+	    command: 'mouseUp'
+	    , target: args.loc
+	});
+	return commands;
+    }
+});
+
+manager.addRollupRule({
+    name: 'verify_text'
+    , description: 'Verify the saved string.'
+    , args: [
+	{
+	    name: 'type'
+	    , description: 'Save as Approved or NeedReview'
+	    , defaultValues: [ 'Approved', 'Fuzzy', 'New' ]
+	}
+	,{
+	    name: 'row'
+            , description: 'Row number, start from  1, -1 is the last row, 0 is current row'
+            , defaultValues: [ -1, 0, 1, 2, 3]
+        }
+        ,{
+	    name: 'text'
+            , description: 'String to be matched'
+            , defaultValues: []
+        }
+    ]
+    , commandMatchers: []
+    , getExpandedCommands: function(args) {
+	var commands = [];
+	var row=args.row;
+
+	commands.push({
+	    command: 'storeAttribute'
+	    , target: webtranPages_get_locator(row)+'@class'
+	    , value: 'rowDecorators'
+	});
+
+	commands.push({
+	    command: 'verifyEval'
+	    , target: 'javascript{storedVars["rowDecorators"].contains("'+ type +'StateDecoration")}'
+	    , value: 'true'
+	});
+
+	if (args.str.length == 0){
+	    commands.push({
+		command: 'verifyElementPresent'
+		, target: 'ui=webtransPages::targetCell(row=' + row +')->//div[@class="TableEditorContent-Empty"]'
+	    });
+	}else{
+	    commands.push({
+		command: 'verifyText'
+		, target: 'ui=webtransPages::targetCell(row=' + row +')'
+		, value: args.text
+	    });
+	}
+        return commands;
+    }
+});
+
+
+manager.addRollupRule({
+    name: 'save_mouse'
+    , description: 'Save with mouse'
+    , args: [
+	{
+	    name: 'type'
+	    , description: 'Save as Approved or NeedReview'
+	    , defaultValues: [ 'Approved', 'Fuzzy', 'Cancel' ]
+        }
+	,{
+	    name: 'row'
+	    , description: 'Row number, start from  1, -1 is the last row, 0 is current row'
+	    , defaultValues: [ -1, 0, 1, 2, 3]
+	}
+        ,{
+	    name: 'text'
+            , description: 'String to be saved'
+            , defaultValues: []
+        }
+    ]
+    , commandMatchers: []
+    , getExpandedCommands: function(args) {
+	/*
+	 * 1. Type
+	 * 2. Save
+	 * 3. Verify
+	 */
+	var commands = [];
+	var row=args.row;
+	var target='ui=webtransPages::targetCell(row=' + row +')';
+
+	commands.push({
+	    command:  'storeText'
+	    , target: target
+	    , value: 'origText' //original text
+	});
+
+	commands.push({
+	    command:  'storeValue'
+	    , target: 'ui=webtransPages::pageEntry()'
+	    , value: 'origPageNum'
+	});
+
+	commands.push({
+	    command:  'click'
+	    , target: target
+	});
+
+	var targetTextarea=target + '->//textarea';
+
+	commands.push({
+	    command:  'waitForElementPresent'
+	    , target: targetTextarea
+	});
+
+	commands.push({
+	    command:  'type'
+	    , target: targetTextarea
+	    , value: args.str
+	});
+
+	var button='ui=webtransPages::messageRow(row=' +row +')->//div[starts-with(@title,"Save as Approved")]';
+	if (args.type=='Fuzzy'){
+	    button='ui=webtransPages::messageRow(row=' +row +')->//div[starts-with(@title,"Save as Fuzzy")]';
+	}else if (args.type=='Cancel'){
+	    button='ui=webtransPages::messageRow(row=' +row +')->//div[starts-with(@title,"Cancel")]';
+	}
+
+	commands.push({
+	    command: 'rollup'
+	    , target: 'sim_click'
+	    , value: 'loc=' + button
+	});
+
+
+	/* Verify the action */
+	if (args.type=='Approved'){
+	    if (row==-1){
+		commands.push({
+		    command: 'click'
+		    ,target: 'css=a[title^="Previous Pages"]'
+		});
+		commands.push({
+		    command: 'waitForValue'
+		    ,target: 'ui=webpages::pageEntry'
+		    ,value: '${origNum}'
+		});
+	    }
+	}else if (args.type=='NeedReview'){
+	}else{ // args.type=='Cancel'
+	}
+
+	commands.push({
+	    command: 'rollup'
+	    ,target: 'verify_text'
+	    ,value: 'type='+ ((args.text.length==0)? 'New' : args.type) + ' row=' + row + ' text=' +args.text
+	});
+        return commands;
     }
 });
 
