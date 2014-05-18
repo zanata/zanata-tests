967dab6cd6131a1abe2205cd925816b2b4834774
Ding-Yi Chen <dingyichen@gmail.com>
2014-05-19 09:58:58 +1000

    mvn_maintainer_main_workflow.sh: New script

---
+++ client-tests/mvn_maintainer_main_workflow.sh
@@ -0,0 +1,27 @@
+#!/bin/sh -x
+PRJ=$1
+VER=$2
+PUSH_TYPE=$3
+shift 3
+
+if [ -n "${PUSH_TYPE}" ];then
+    PUSH_OPTS="-Dzanata.pushType=${PUSH_TYPE}"
+else
+    PUSH_OPTS=""
+fi
+
+if [ -n "${ZANATA_URL}" ];then
+    AUTH_OPTS+=-Dzanata.url=${ZANATA_URL}
+fi
+if [ -n "${ZANATA_USERNAME}" ];then
+    AUTH_OPTS+= -Dzanata.username=${ZANATA_USERNAME}
+fi
+if [ -n "${ZANATA_KEY}" ];then
+    AUTH_OPTS+= -Dzanata.key=${ZANATA_KEY}
+fi
+
+mvn -B -e zanata:put-version ${AUTH_OPTS} -Dzanata.versionProject=${PRJ} -Dzanata.versionSlug=${VER} 
+wget -O zanata.xml "${ZANATA_URL}iteration/view/${PRJ}/${VER}?cid=77&actionMethod=iteration%2Fview.xhtml%3AconfigurationAction.downloadGeneralConfig%28%29" 
+mvn -B -e zanata:push ${AUTH_OPTS} ${PUSH_OPTS} $@
+mvn -B -e zanata:pull ${AUTH_OPTS} $@
+
