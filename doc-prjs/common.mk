f5c4c30a1166c2b0daf45fb57c995febaa5791d2
Ding-Yi Chen <dingyichen@gmail.com>
2014-05-27 10:04:40 +1000

    doc-prjs: add zanata

--- doc-prjs/common.mk
+++ doc-prjs/common.mk
@@ -5,12 +5,17 @@ clean:
 
 ifeq ($(PRJ_TYPE), podir)
 $(VER_TASKS): %/pot/: %/
-	if [ -x /usr/bin/publican ]; then \
-	    if [ -e $*/publican.cfg ];then \
-	    sed -e "s/brand:.*//" $*/publican.cfg > $*/publican.cfg.stripped ;\
-	    cd $*; publican update_pot --config $*/publican.cfg.stripped; \
-	    publican update_po --config $*/publican.cfg.stripped  --langs=`find . -maxdepth 1 -mindepth 1 -type d ! -name "pot" ! -name "\.git" | xargs | sed -e 's/ /,/g'` ;\
-	    fi;\
-	fi
+
 endif
+
+# publican no longer required, as the revnumber in older projects 
+# are no longer valid.
+# 	
+# if [ -x /usr/bin/publican ]; then
+#    if [ -e $*/publican.cfg ];then 
+#       sed -e "s/brand:.*//" $*/publican.cfg > $*/publican.cfg.stripped
+#       cd $*; publican update_pot --config publican.cfg.stripped
+#       publican update_po --config publican.cfg.stripped  --langs=`find . -maxdepth 1 -mindepth 1 -type d ! -name "pot" ! -name "\.git" | xargs | sed -e 's/ /,/g'`
+#    fi
+# fi
 
