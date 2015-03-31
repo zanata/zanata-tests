90d0f2b3b370d2c5eba96cdd1e8aca3952234d2f
Ding-Yi Chen <dingyichen@gmail.com>
2015-03-31 20:37:27 +1000

    RHBZ1172168

--- bugs/RHBZ1172618/query.sh
+++ bugs/RHBZ1172618/query.sh
@@ -14,7 +14,7 @@ if [ $# -le 0 ]; then
 fi
 
 ok=1
-for k in ZANATA_USERNAME ZANATA_KEY;do
+for k in ZANATA_URL ZANATA_USERNAME ZANATA_KEY;do
     eval "v=\${${k}}"
     if [ -z "$v" ];then
          ok=0
@@ -43,14 +43,17 @@ function curl_tests(){
         curlOpts+=(-H "Accept:$accept")
     fi
 
+    ### Valid Auth
     curl -v -X $method -H "X-Auth-User:${ZANATA_USERNAME}" -H "X-Auth-Token:${ZANATA_KEY}" "${curlOpts[@]}" "${ZANATA_URL}rest${restPath}"
 
     echo -e "\n-----------------"
 
+    ### No Auth
     curl -v -X $method "${curlOpts[@]}" "${ZANATA_URL}rest${restPath}"
 
     echo -e "\n================="
 
+    ### Invalid auth
     curl -v -X $method -H "X-Auth-User:${ZANATA_USERNAME}" -H "X-Auth-Token:1" "${curlOpts[@]}" "${ZANATA_URL}rest${restPath}"
 }
 
